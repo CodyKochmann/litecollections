a293421fcf58e05b7dcac8ad62bbcae0e10b7a8e
Cody Kochmann <kochmanncody@gmail.com>
2022-05-22 17:57:28 +0000

    added a failing test for LiteSets implementation

---
+++ litecollections/tests/test_LiteSet.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from functools import partial
+from unittest import TestCase, main
+from random import getrandbits
+
+from hypothesis import given
+from hypothesis.strategies import text, integers
+
+from litecollections import LiteSet
+
+''' Main unittests for litecollections.LiteSet '''
+
+def random_bytes():
+    return getrandbits(128).to_bytes(16, 'little')
+
+class Test_LiteSet(TestCase):
+    ''' Main unittests for litecollections.LiteSet '''
+    def test_all_methods_are_implemented(self):
+        '''tests if all methods sets have are defined in LiteSet'''
+        set_to_mirror = set()
+        methods_of_a_set = {i for i in dir(set_to_mirror) if callable(getattr(set_to_mirror, i))}
+        with LiteSet() as s:
+            methods_of_a_LiteSet = {i for i in dir(s) if callable(getattr(s, i))}
+            self.assertSetEqual(methods_of_a_set, methods_of_a_LiteSet)
+        
+    def test_int_members(self):
+        '''test if ints can be used as members'''
+        with LiteSet() as s:
+            for k in range(10):
+                self.assertNotIn(k, s)
+                self.assertEqual(k, len(s))
+                s.add(k)
+                self.assertIn(k, s)
+                self.assertEqual(k+1, len(s))
+            for i in s:
+                self.assertIsInstance(i, int)
+
+    def test_str_members(self):
+        '''test if ints can be used as members'''
+        with LiteSet() as s:
+            for _k in range(10):
+                k = str(_k)
+                self.assertNotIn(k, s)
+                self.assertEqual(_k, len(s))
+                s.add(k)
+                self.assertIn(k, s)
+                self.assertEqual(_k+1, len(s))
+            for i in s:
+                self.assertIsInstance(i, str)
+
+    def test_byte_members(self):
+        '''test if ints can be used as members'''
+        with LiteSet() as s:
+            for _k in range(10):
+                k = random_bytes()
+                self.assertNotIn(k, s)
+                self.assertEqual(_k, len(s))
+                s.add(k)
+                self.assertIn(k, s)
+                self.assertEqual(_k+1, len(s))
+            for i in s:
+                self.assertIsInstance(i, str)
+    
+    def test_length_functionality(self):
+        '''tests if LiteSet mirrors what a set would do for len()'''
+        with LiteSet() as s:
+            test_string = 'hello world'
+            set_to_mirror = set()
+            for nth, letter in enumerate(test_string):
+                s.add(letter)
+                self.assertIn(letter, s)
+                set_to_mirror.add(letter)
+                self.assertIn(letter, set_to_mirror)
+                self.assertEqual(len(set_to_mirror), len(s))
+
+
+class Test_LiteSet_HypthesisBeatdown(TestCase):
+    def generate_type_test(self, member_strategy):
+        '''acts as a harness for type hypothesis to try different type combos'''
+        assert callable(member_strategy), member_strategy
+
+        @given(member_strategy())
+        def test(k):
+            with LiteSet() as s:
+                s.add(k)
+                self.assertIn(k, s)
+                # check if deduplication works
+                len_before = len(s)
+                s.add(k)
+                self.assertEqual(len_before, len(s))
+                # test deletion 
+                s.discard(k)
+                self.assertNotIn(k, d)
+                # reinsert to keep building up a db
+                s.add(k)
+                self.assertIn(k, s)
+        return test
+
+    def test_str_keys_and_str_values(self):
+        '''tests if hypothesis can come up with string members that LiteSet wont work with'''
+        self.generate_type_test(text)()
+    
+    def test_str_keys_and_int_values(self):
+        '''tests if hypothesis can come up with string members that LiteSet wont work with'''
+        self.generate_type_test(integers)()
+
+if __name__ == '__main__':
+    main(verbosity=2)
